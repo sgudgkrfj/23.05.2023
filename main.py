15d57d8719c26d99d38b0394f4a9f018d6fdfd66
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-23 18:56:14 +0300

    запис в .log

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 import logging #стандартна бібліотека для логування-перебігу програми
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(massage)s')
+logging.basicConfig(level=logging.DEBUG,
+                    filename="logs.log",
+                    filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(massage)s')
 logging.debug("debug")
 logging.info("info")
 logging.error("погано написав програму error")
