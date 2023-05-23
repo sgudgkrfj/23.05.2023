22d62ab8ce834f24c5a32ecf50ad98f775ede318
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-23 18:50:53 +0300

    Організація повідомлень

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
-
-
-
+import logging #стандартна бібліотека для логування-перебігу програми
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(massage)s')
+logging.debug("debug")
+logging.info("info")
+logging.error("погано написав програму error")
+logging.warning("warning")
+logging.critical("critical")
